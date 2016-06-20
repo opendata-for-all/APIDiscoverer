87229e0704d14c04d29e161efbc04cab78e0dbc6
hamza <hamza.eddouibi@gmail.com>
2016-06-20 14:39:39 +0200

    generation process

--- apidiscoverer/src/som/apidiscoverer/Generator.java
+++ apidiscoverer/src/som/apidiscoverer/Generator.java
@@ -1,11 +1,17 @@
 package som.apidiscoverer;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-
 import core.APIOperation;
+import core.APIParameter;
 import core.Api;
+import core.Info;
+import core.JsonDataType;
+import core.ParameterLocation;
 import core.Path;
+import core.Response;
 import core.Schema;
+import core.SchemeType;
 
 public class Generator {
 	
@@ -13,10 +19,30 @@ public class Generator {
 	public static JsonObject getJsonFromSwaggerModel(Api api) {
 		JsonObject jsonObject = new JsonObject();
 		jsonObject.addProperty("swagger", api.getSwagger());
+		if(api.getInfo()!= null){
+			JsonObject infoJson = new JsonObject();
+			jsonObject.add("info", infoJson);
+			generateInfo(api.getInfo(),infoJson);
+		}
 		jsonObject.addProperty("host", api.getHost());
 		jsonObject.addProperty("basePath", api.getBasePath());
-		if(api.getInfo()!= null){
-			generateInfo(api,jsonObject);
+		if(!api.getSchemes().isEmpty()){
+			JsonArray schemes = new JsonArray();
+			for(SchemeType scheme: api.getSchemes())
+				schemes.add(scheme.getLiteral());
+			jsonObject.add("schemes", schemes);
+		}
+		if(!api.getConsumes().isEmpty()){
+			JsonArray consumesList = new JsonArray();
+			for(String consume : api.getConsumes())
+				consumesList.add(consume);
+			jsonObject.add("consumes", consumesList);
+		}
+		if(!api.getProduces().isEmpty()){
+			JsonArray producesList = new JsonArray();
+			for(String produces : api.getConsumes())
+				producesList.add(produces);
+			jsonObject.add("produces", producesList);
 		}
 		if(!api.getPaths().isEmpty()){
 			JsonObject jsonPaths = new JsonObject();
@@ -25,18 +51,17 @@ public class Generator {
 			
 		}
 		if(!api.getDefinitions().isEmpty()){
-			JsonObject jsonPaths = new JsonObject();
-			jsonObject.add("definitions",jsonPaths);
-			generateDefinitions(api,jsonPaths);
+			JsonObject jsonDefintions = new JsonObject();
+			jsonObject.add("definitions",jsonDefintions);
+			generateDefinitions(api,jsonDefintions);
 			
 		}
 				return jsonObject;
 
 			}
-	private static void generateInfo(Api api, JsonObject jsonObject) {
-		JsonObject jsonInfo = new JsonObject();
-		jsonInfo.addProperty("title", api.getInfo().getTitle());
-		jsonInfo.addProperty("version", api.getInfo().getVersion());
+	private static void generateInfo(Info info, JsonObject jsonInfo) {
+		jsonInfo.addProperty("title", info.getTitle());
+		jsonInfo.addProperty("version", info.getVersion());
 		
 	}
 			private static void generatePaths(Api api, JsonObject jsonPaths) {
@@ -76,15 +101,128 @@ public class Generator {
 				}
 			}
 			private static void generateOperation(APIOperation operation, JsonObject jsonOperation) {
-//				jsonOperation.addProperty(property, value);
+				if(!operation.getTags().isEmpty()){
+					JsonArray tags = new JsonArray();
+					for(String tag: operation.getTags())
+						tags.add( tag);
+					jsonOperation.add("tags", tags);
+				}
+				if(operation.getSummary()!= null){
+					jsonOperation.addProperty("summary", operation.getSummary());
+				}
+				if(operation.getDescription()!= null){
+					jsonOperation.addProperty("description", operation.getDescription());
+				}
+				//TODO externalDocs
+				//TODO operationId
+				if(!operation.getConsumes().isEmpty()){
+					JsonArray consumesList = new JsonArray();
+					for(String consume : operation.getConsumes())
+						consumesList.add(consume);
+					jsonOperation.add("consumes", consumesList);
+				}
+				if(!operation.getProduces().isEmpty()){
+					JsonArray producesList = new JsonArray();
+					for(String produces : operation.getConsumes())
+						producesList.add(produces);
+					jsonOperation.add("produces", producesList);
+				}
+				if(!operation.getParameters().isEmpty()){
+					JsonArray paramatersJson = new JsonArray();
+					for(APIParameter parameter : operation.getParameters()){
+						JsonObject parameterJson = new JsonObject();
+						paramatersJson.add(parameterJson);
+						generateParameter(parameter,parameterJson);
+					}
+					jsonOperation.add("parameters", paramatersJson);
+				}
+				if(!operation.getResponses().isEmpty()){
+					JsonObject responsesJson = new JsonObject();
+					
+					for(Response response :operation.getResponses()){
+						JsonObject responseJson = new JsonObject();
+						responsesJson.add(response.getCode(), responseJson);
+						generateResponse (response,responseJson);
+					}
+				}
+				if(!operation.getSchemes().isEmpty()){
+					JsonArray schemes = new JsonArray();
+					for(SchemeType scheme: operation.getSchemes())
+						schemes.add(scheme.getLiteral());
+					jsonOperation.add("schemes", schemes);
+				}
+				//TODO deprecated
+				//TODO security
 			
 			}
-			private static void generateDefinitions(Api api, JsonObject jsonPaths) {
+			private static void generateParameter(APIParameter parameter, JsonObject parameterJson) {
+				parameterJson.addProperty("name", parameter.getName());
+				parameterJson.addProperty("in",parameter.getIn().getLiteral());
+				if(parameter.getDescription()!= null)
+					parameterJson.addProperty("description", parameter.getDescription());
+				parameterJson.addProperty("required", parameter.isRequired());
+				if(parameter.getIn().equals(ParameterLocation.BODY)){
+					if(parameter.getSchema().getType().equals(JsonDataType.OBJECT))
+						parameterJson.addProperty("schema", "#/definitions/"+parameter.getSchema().getName());
+					else {
+						//TODO array and primitive
+					}
+				}
+					
+			}
+			private static void generateResponse(Response response, JsonObject responseJson) {
+				responseJson.addProperty("description", response.getDescription());
+				if(response.getSchema()!= null){
+					if(response.getSchema().getType().equals(JsonDataType.OBJECT))
+						responseJson.addProperty("schema", "/#/definitions/"+response.getSchema().getName());
+					else {
+						//TODO array and primitive
+					}
+						
+				}
+			
+				
+			}
+			private static void generateDefinitions(Api api, JsonObject jsonDefinitions) {
 				for(Schema schema: api.getDefinitions()){
-					JsonObject jsonProperties = new JsonObject();
-					jsonPaths.add(schema.getName(), jsonProperties);
+					JsonObject schemaJson = new JsonObject();
+					jsonDefinitions.add(schema.getName(), schemaJson);
+					generateSchema(schema,schemaJson);
 				}
 				
+			}
+			private static void generateSchema(Schema schema, JsonObject schemaJson) {
+				schemaJson.addProperty("type", schema.getType().getLiteral());
+				if(!schema.getProperties().isEmpty()){
+					JsonObject propertiesJson = new JsonObject();
+				schemaJson.add("properties", propertiesJson );
+				for(Schema property : schema.getProperties()){
+					JsonObject propertyJson = new JsonObject();
+					propertiesJson.add(property.getName(), propertyJson);
+					generateSchemaProperty(property, propertyJson);
+				}
+					
+				}
+			}
+			private static void generateSchemaProperty(Schema property, JsonObject propertyJson) {
+				if(property.getType().equals(JsonDataType.OBJECT)){
+					propertyJson.addProperty("$ref", "#/definitions/"+property.getRefResolved().getName());
+				}else {
+					if(property.getType().equals(JsonDataType.ARRAY)){
+						propertyJson.addProperty("type", JsonDataType.ARRAY.getLiteral());
+						JsonObject items = new JsonObject();
+						if(property.getItems().getType().equals(JsonDataType.OBJECT))
+							items.addProperty("$ref", "#/definitions/"+property.getItems().getName());
+						else 
+							items.addProperty("type", property.getItems().getType().getLiteral());
+						propertyJson.add("items", items);
+						
+						
+					}
+					else {
+						propertyJson.addProperty("type", property.getType().getLiteral());
+					}
+				}
 			}
 
 }
